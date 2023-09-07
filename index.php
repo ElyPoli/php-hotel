65a9c0396e27f6fde0d9bc0ac419d559355fec0d
ElyPoli <itsos.poli@gmail.com>
2023-09-07 15:08:03 +0200

    added basic printing of hotels array

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
-
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +57,17 @@
 </head>
 
 <body>
+
+    <?php foreach ($hotels as $single_hotel) { ?>
+        <?php foreach ($single_hotel as $element) { ?>
+            <p>
+                <?php echo $element ?>
+            </p>
+        <?php } ?>
+    <?php } ?>
+
+
+
 </body>
 
 </html>
